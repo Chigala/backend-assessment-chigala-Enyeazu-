436e49ec0ba449f7b9b92a406e03c5e4df08f559
Chigala <92148630+Chigala@users.noreply.github.com>
2022-10-11 18:17:34 +0100

    added new set of tests
    added the transaction and auth test for the project

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import App from "@/app";
 import AuthRoute from "@/routes/auth.route";
 
+const app = new App([new AuthRoute()]);
 afterAll(async () => {
   await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
 });
@@ -10,61 +11,67 @@ describe("Testing Auth", () => {
   describe("[POST] /signup", () => {
     it("response should have the Create userData", done => {
       const userData = {
-        email: "chigalatheboss@gmail.com",
-        password: "chigala2000",
-        name: "chigalaman",
+        email: "test700@gmail.com",
+        name: "hellojki",
       };
 
-      const authRoute = new AuthRoute();
-      const app = new App([authRoute]);
       request(app.getServer())
         .post("/signup")
         .send(userData)
         .set("Accept", "application/json")
         .expect("Content-Type", "application/json; charset=utf-8")
-        .expect(response => {
-          console.log(response);
-        })
-        .expect(201, done);
+        .expect(400, done);
     });
-
-    // it("check if the user already exists ", () => {
-    //   const userData = {
-    //     email: "test@gmail.com",
-    //     password: "chigala",
-    //     name: "chigala",
-    //   };
-    //   const authRoute = new AuthRoute();
-    //   const app = new App([authRoute]);
-    //   return request(app.getServer()).post("/signup").expect("Content-Type", /json/).send(userData).expect(409);
-    // });
+    it("checks if the userData contains a username ", done => {
+      const userData = {
+        email: "test@gmail.com ",
+        password: "12345678",
+      };
+      request(app.getServer())
+        .post("/signup")
+        .send(userData)
+        .set("Accept", "application/json")
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .expect(400, done);
+    });
+    it("checks if the userData contains a password ", done => {
+      const userData = {
+        email: "test@gmail.com",
+        username: "hello",
+      };
+      request(app.getServer())
+        .post("/signup")
+        .send(userData)
+        .set("Accept", "application/json")
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .expect(400, done);
+    });
   });
 
-  // describe("[POST] /login", () => {
-  //   it("response should have the Set-Cookie header with the Authorization token", async () => {
-  //     const userData = {
-  //       email: "test@email.com",
-  //       password: "q1w2e3r4",
-  //     };
-
-  //     const authRoute = new AuthRoute();
-  //     const app = new App([authRoute]);
-  //     return request(app.getServer())
-  //       .post("/login")
-  //       .send(userData)
-  //       .expect("Set-Cookie", /^Authorization=.+/);
-  //   });
-  // });
+  describe("[POST] /login", () => {
+    it("response should have the Set-Cookie header with the Authorization token", async () => {
+      const userData = {
+        email: "test@email.com",
+        password: "q1w2e3r4",
+      };
+      request(app.getServer())
+        .post("/login")
+        .set("Accept", "application/json")
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .send(userData)
+        .expect("Set-Cookie", /^Authorization=.+/);
+    });
+  });
 
   // error: StatusCode : 404, Message : Authentication token missing
-  // describe('[POST] /logout', () => {
-  //   it('logout Set-Cookie Authorization=; Max-age=0', () => {
+  // describe("[POST] /logout", () => {
+  //   it("logout Set-Cookie Authorization=; Max-age=0", () => {
   //     const authRoute = new AuthRoute();
   //     const app = new App([authRoute]);
 
   //     return request(app.getServer())
-  //       .post('/logout')
-  //       .expect('Set-Cookie', /^Authorization=\;/);
+  //       .post("/logout")
+  //       .expect("Set-Cookie", /^Authorization=\;/);
   //   });
   // });
 });
--- src/tests/transactions.test.ts
+++ src/tests/transactions.test.ts
@@ -1,5 +1,39 @@
-// describe("Testing Transactions", () => {
-//   describe("[Post]/senderId/receiverId", () => {
-//     it()
-//   });
-// });
+import request from "supertest";
+import App from "@/app";
+import TransactionRoute from "@/routes/transactions.route";
+
+const app = new App([new TransactionRoute()]);
+afterAll(async () => {
+  await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
+});
+
+describe("Testing transactions", () => {
+  describe("[Get] /transactions", () => {
+    it("it should show all transactions", () => {
+      const transactions = new TransactionRoute();
+      request(app.getServer()).get(`${transactions.path}`).expect(200);
+    });
+    it("it should show one transaction", () => {
+      const transactions = new TransactionRoute();
+      request(app.getServer()).get(`${transactions.path}/1`).expect(200);
+    });
+    it("if no transaction found", () => {
+      const transactions = new TransactionRoute();
+      request(app.getServer()).get(`${transactions.path}/1000`).expect(404);
+    });
+  });
+
+  describe("[Post] /transactions", () => {
+    it("fund account", () => {
+      const transactions = new TransactionRoute();
+      request(app.getServer())
+        .post(`${transactions.path}`)
+        .send({
+          type: "credit",
+          amount: 1000,
+          accountNumber: 1,
+        })
+        .expect(201);
+    });
+  });
+});
